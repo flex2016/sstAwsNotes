931b5a1a9b521ab7aa118e69e8b84055002c5c1c
Kurilo <taras.kurilo@cgi.com>
2021-10-28 12:42:44 -0400

    added frontend and login

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,10 +3,14 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 
 export default function Routes() {
   return (
     <Switch>
+      <Route exact path="/login">
+        <Login />
+      </Route>
       <Route exact path="/">
         <Home />
       </Route>
