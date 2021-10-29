ae506c53a0ffba4d84d507e9a4d88f7211c5470d
Kurilo <taras.kurilo@cgi.com>
2021-10-29 15:02:00 -0400

    added sign up reset password

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,10 +4,15 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import ResetPassword from "./containers/ResetPassword";
+import Signup from "./containers/Signup";
 
 export default function Routes() {
   return (
     <Switch>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
       <Route exact path="/login">
         <Login />
       </Route>
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
 import { Auth } from "aws-amplify";
+import Form from "react-bootstrap/Form";
 import { useHistory } from "react-router-dom";
-
-import { useAppContext } from "../lib/contextLib";
 import LoaderButton from "../components/LoaderButton";
+import { useAppContext } from "../lib/contextLib";
+import { useFormFields } from "../lib/hooksLib";
 import { onError } from "../lib/errorLib";
 import "./Login.css";
 
 export default function Login() {
-  const { userHasAuthenticated } = useAppContext();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
   const history = useHistory();
+  const { userHasAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: ""
+  });
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
   async function handleSubmit(event) {
@@ -26,7 +27,7 @@ export default function Login() {
     setIsLoading(true);
 
     try {
-      await Auth.signIn(email, password);
+      await Auth.signIn(fields.email, fields.password);
       userHasAuthenticated(true);
       history.push("/");
     } catch (e) {
@@ -34,6 +35,7 @@ export default function Login() {
       setIsLoading(false);
     }
   }
+
   return (
     <div className="Login">
       <Form onSubmit={handleSubmit}>
@@ -42,16 +44,16 @@ export default function Login() {
           <Form.Control
             autoFocus
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </Form.Group>
         <Form.Group size="lg" controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
           />
         </Form.Group>
         <LoaderButton
