7a86398ea9f2413e4747d288cf4f00562cd2da9e
Kurilo <reloded2004@hotmail.com>
2021-11-01 14:34:31 -0400

    added code splitting

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,52 +1,66 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import Home from "./containers/Home";
-import NotFound from "./containers/NotFound";
-import Login from "./containers/Login";
-import ResetPassword from "./containers/ResetPassword";
-import Signup from "./containers/Signup";
-import NewNote from "./containers/NewNote";
-import Notes from "./containers/Notes";
-import Settings from "./containers/Settings";
-import ChangePassword from "./containers/ChangePassword";
-import ChangeEmail from "./containers/ChangeEmail";
-
+import asyncComponent from "./components/AsyncComponent";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+
+// import Home from "./containers/Home";
+// import NotFound from "./containers/NotFound";
+// import Login from "./containers/Login";
+// import ResetPassword from "./containers/ResetPassword";
+// import Signup from "./containers/Signup";
+// import NewNote from "./containers/NewNote";
+// import Notes from "./containers/Notes";
+// import Settings from "./containers/Settings";
+// import ChangePassword from "./containers/ChangePassword";
+// import ChangeEmail from "./containers/ChangeEmail";
+
+const AsyncHome = asyncComponent(() => import("./containers/Home"));
+const AsyncSettings= asyncComponent(() => import("./containers/Settings"));
+const AsyncLogin = asyncComponent(() => import("./containers/Login"));
+const AsyncNotes = asyncComponent(() => import("./containers/Notes"));
+const AsyncSignup = asyncComponent(() => import("./containers/Signup"));
+const AsyncNewNote = asyncComponent(() => import("./containers/NewNote"));
+const AsyncResetPassword = asyncComponent(() => import("./containers/ResetPassword"));
+const AsyncChangePassword = asyncComponent(() => import("./containers/ChangePassword"));
+const AsyncChangeEmail = asyncComponent(() => import("./containers/ChangeEmail"));
+const AsyncNotFound = asyncComponent(() => import("./containers/NotFound"));
+
+
 
 export default function Routes() {
   return (
     <Switch>
       <UnauthenticatedRoute exact path="/login/reset">
-        <ResetPassword />
+        <AsyncResetPassword />
       </UnauthenticatedRoute>
       <UnauthenticatedRoute exact path="/login">
-        <Login />
+        <AsyncLogin />
       </UnauthenticatedRoute>
       <UnauthenticatedRoute exact path="/signup">
-        <Signup />
+        <AsyncSignup />
       </UnauthenticatedRoute>
       <AuthenticatedRoute exact path="/settings/password">
-        <ChangePassword />
+        <AsyncChangePassword />
       </AuthenticatedRoute>
       <AuthenticatedRoute exact path="/settings/email">
-        <ChangeEmail />
+        <AsyncChangeEmail />
       </AuthenticatedRoute>
       <AuthenticatedRoute exact path="/settings">
-        <Settings />
+        <AsyncSettings />
       </AuthenticatedRoute>
       <AuthenticatedRoute exact path="/notes/new">
-        <NewNote />
+        <AsyncNewNote />
       </AuthenticatedRoute>
       <AuthenticatedRoute exact path="/notes/:id">
-        <Notes />
+        <AsyncNotes />
       </AuthenticatedRoute>
       <Route exact path="/">
-        <Home />
+        <AsyncHome />
       </Route>
       <Route>
-        <NotFound />
+        <AsyncNotFound />
       </Route>
     </Switch>
   );
